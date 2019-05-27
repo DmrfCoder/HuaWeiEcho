219194b03f39d799de1929c0e08e4687cf0dfd9b
Joooey <664999006@qq.com>
2019-05-27 20:49:23 +0800

    通信接收url和下载文件

--- Server/HuaWeiEcho/model.py
+++ Server/HuaWeiEcho/model.py
@@ -7,4 +7,19 @@ def model_recogonize_music_of_tiktok_video(maybeUrl):
     url = re.findall(pattern, maybeUrl)
     return url
 
+def recognize_music(url):
+    """
+    通过url识别音乐
+    返回结果list
+    :param url: 解析得到的url
+    """
+def extract_music(url):
+    """
+    通过url获取音频文件并下载到服务器
+    返回暂存地址
+    :param url: 解析得到的url
+    """
+    pass
+
+
 
